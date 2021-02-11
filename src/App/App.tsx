ae585cceecfd522590de0a724a413f24da1a1c45
Nafiz Ahmed <redoyahmed11@gmail.com>
2021-02-11 18:28:58 +0600

    Fetch user info on logged in

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,18 +1,49 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import { Store } from 'redux';
 import Login from '../components/login/Login';
 import {
+  GITHUB_USER_ENDPOINT,
   LOCAL_AUTHORIZE_ENDPOINT,
   LOCAL_HOME_ENDPOINT,
   LOCAL_LOGIN_ENDPOINT,
 } from '../configs/endpoints';
+import ConnectedSessionAuthorize from '../containers/sessionAuthorize/SessionAuthorize';
+import { axioService, GET } from '../services/axioService';
+import { getSessionToken, setSessionUserInfo } from '../store/ducks/session';
 import './App.scss';
 
-const App: React.FC = () => {
+interface AppProps {
+  token: string;
+  setSessionUserInfoActionCreator: typeof setSessionUserInfo;
+}
+
+const App: React.FC<AppProps> = (props: AppProps) => {
+  const { token, setSessionUserInfoActionCreator } = props;
+
+  React.useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await axioService(GET, GITHUB_USER_ENDPOINT, {}, true);
+        setSessionUserInfoActionCreator(response.data);
+      } catch (exception) {
+        /** console error the exception */
+        console.error(exception);
+      }
+    };
+
+    if (token !== '') {
+      fetchProfile();
+    }
+  }, [token]);
+
   return (
     <React.Fragment>
       <Switch>
-        <Route path={LOCAL_AUTHORIZE_ENDPOINT}>Authorize</Route>
+        <Route path={LOCAL_AUTHORIZE_ENDPOINT}>
+          <ConnectedSessionAuthorize />
+        </Route>
         <Route path={LOCAL_LOGIN_ENDPOINT}>
           <Login />
         </Route>
@@ -25,4 +56,26 @@ const App: React.FC = () => {
   );
 };
 
-export default App;
+/** Interface to describe props from mapStateToProps */
+interface DispatchedStateProps {
+  token: string;
+}
+
+/** Map props to state  */
+const mapStateToProps = (state: Partial<Store>): DispatchedStateProps => {
+  const result = {
+    token: getSessionToken(state),
+  };
+  return result;
+};
+
+/** map props to actions */
+const mapDispatchToProps = {
+  setSessionUserInfoActionCreator: setSessionUserInfo,
+};
+
+/** connect App to the redux store */
+const ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(App);
+
+/** the default export */
+export default ConnectedApp;
