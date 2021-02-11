8f6afba638d569bb4bc6b92d24ed0a0d7f32f8fe
Nafiz Ahmed <redoyahmed11@gmail.com>
2021-02-11 18:28:39 +0600

    Connect store with the app

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,16 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
