d3a73f7866684ab4c40ba4e922da746833747823
Nafiz Ahmed <redoyahmed11@gmail.com>
2021-02-11 23:03:57 +0600

    Render dashboard on home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import ConnectedDashboard from '../../containers/dashboard/Dashboard';
 import withLayout from '../../hocs/withLayout/withLayout';
 import './Home.scss';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="Home-container">
+      <ConnectedDashboard />
+    </div>
+  );
 };
 
 export default withLayout(Home);
