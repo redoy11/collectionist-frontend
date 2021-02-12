c26819e3acbc0301106ba5a6b1f27b97b1b882c7
Nafiz Ahmed <redoyahmed11@gmail.com>
2021-02-12 19:44:23 +0600

    Add put axio service

--- src/services/axioService.ts
+++ src/services/axioService.ts
@@ -16,6 +16,10 @@ type GET = typeof GET;
 export const POST = 'POST';
 type POST = typeof POST;
 
+/** PUT REQUEST TYPE */
+export const PUT = 'PUT';
+type PUT = typeof PUT;
+
 /** DELETE REQUEST TYPE */
 export const DELETE = 'DELETE';
 type DELETE = typeof DELETE;
@@ -78,6 +82,33 @@ export const axioPost = async (
 };
 
 /**
+ * axioPut makes a put request using the axio libary
+ * @param {string} url - the url where the put request will be made
+ * @param {FlexObj | FlexObj[]} params - the parameters that will be passed
+ * @param {boolean} isProtected - if true, access token will be included in header; otherwise, not.
+ * @returns {any} - response returned by the server if successful
+ */
+export const axioPut = async (
+  url: string,
+  params: FlexObj | FlexObj[] = {},
+  isProtected = false
+) => {
+  /** retrieve the access token from redux store */
+  const token = getSessionToken((store as any).getState());
+
+  const config = isProtected
+    ? {
+        headers: {
+          Accept: `application/json`,
+          Authorization: `token ${token}`,
+        },
+      }
+    : { headers: { Accept: `application/json` } };
+  const response = await axios.put(url, params, config);
+  return await response;
+};
+
+/**
  * axioDelete makes a delete request using the axio libary
  * @param {string} url - the url where the delete request will be made
  * @param {boolean} isProtected - if true, access token will be included in header; otherwise, not.
@@ -108,7 +139,7 @@ export const axioDelete = async (url: string, isProtected = false) => {
  * @returns {any} - response returned by the server if successful; otherwise, error object
  */
 export const axioService = async (
-  type: GET | POST | DELETE,
+  type: GET | POST | PUT | DELETE,
   url: string,
   params: FlexObj | FlexObj[] = {},
   isProtected = false
@@ -117,6 +148,8 @@ export const axioService = async (
   try {
     if (type === POST) {
       response = await axioPost(url, params, isProtected);
+    } else if (type === PUT) {
+      response = await axioPut(url, params, isProtected);
     } else if (type === DELETE) {
       response = await axioDelete(url, isProtected);
     } else {
