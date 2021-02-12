80708855e44749f563b355cb96aab2ea148aaf11
Nafiz Ahmed <redoyahmed11@gmail.com>
2021-02-12 19:46:45 +0600

    Connect the add collection with the server api

--- src/containers/addCollection/AddCollection.tsx
+++ src/containers/addCollection/AddCollection.tsx
@@ -7,11 +7,14 @@ import {
   getCollectionById,
   setCollection,
 } from '../../store/ducks/collections';
-import { v4 as uuid } from 'uuid';
 import './AddCollection.scss';
+import { axioService, POST, PUT } from '../../services/axioService';
+import { SERVER_COLLECTIONS_ENDPOINT } from '../../configs/endpoints';
+import { getSessionUserInfo } from '../../store/ducks/session';
 
 /** interface to describe AddCollection props */
 interface AddCollectionProps {
+  userInfo: any;
   visible: boolean;
   setCollectionActionCreator: typeof setCollection;
   closeHandler: () => void;
@@ -29,9 +32,13 @@ const AddCollection: React.FC<AddCollectionProps> = (
     closeHandler,
     setCollectionActionCreator,
     collectionInfo,
+    userInfo,
   } = props;
+
+  /** manages the title input state */
   const [title, setTitle] = React.useState<string>('');
 
+  /** initialize modal on visible */
   React.useEffect(() => {
     if (collectionInfo) {
       setTitle(collectionInfo.title);
@@ -48,20 +55,34 @@ const AddCollection: React.FC<AddCollectionProps> = (
     setTitle(event.target.value);
   };
 
-  const okHandler = () => {
+  /** manages the create/edit request  */
+  const okHandler = async () => {
     if (title === '') return;
     if (collectionInfo) {
+      const response = await axioService(
+        PUT,
+        `${SERVER_COLLECTIONS_ENDPOINT}/${userInfo.id}/${collectionInfo.id}`,
+        { title },
+        true
+      );
       setCollectionActionCreator({
         ...collectionInfo,
+        updatedAt: response.data[0].updated_at || '',
         title,
       });
     } else {
+      const response = await axioService(
+        POST,
+        `${SERVER_COLLECTIONS_ENDPOINT}/${userInfo.id}`,
+        { title },
+        true
+      );
       setCollectionActionCreator({
-        id: uuid(),
+        id: response.data[0].id.toString(),
         title,
         type: '',
-        createdAt: '',
-        updatedAt: '',
+        createdAt: response.data[0].created_at,
+        updatedAt: response.data[0].updated_at,
       });
     }
     closeHandler();
@@ -105,6 +126,7 @@ const AddCollection: React.FC<AddCollectionProps> = (
 
 /** Interface to describe props from mapStateToProps */
 interface DispatchedStateProps {
+  userInfo: any;
   collectionInfo: CollectionObj | null;
 }
 
@@ -118,6 +140,7 @@ const mapStateToProps = (
 ): DispatchedStateProps => {
   const result = {
     collectionInfo: getCollectionById(state, parentProps?.collectionId || ''),
+    userInfo: getSessionUserInfo(state),
   };
   return result;
 };
